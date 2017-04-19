b70e5ad04cfba27540c7c3574741ab82c2870e63
杨柳 <1545766219@qq.com>
2017-04-19 09:08:52 +0800

    拷贝leancloud 初始化代码

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import  * as localStore from './localStore'
+import AV from 'leancloud-storage'
+
+var APP_ID = 'ttfT4FPkoQ5uSAOWVBy6BT9z-gzGzoHsz';
+var APP_KEY = 'RR4cpVY1AiF5y2yXKGGbdOKd';
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY,
+});
 
 class App extends Component {
   constructor(props){
