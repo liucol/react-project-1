cb8b552fd47a452e95df237df5ce65a74b4e955a
杨柳 <1545766219@qq.com>
2017-04-19 15:16:30 +0800

    添加登录/注册对话框UserDialog

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './reset.css'
 import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
-
+import UserDialog from './UserDialog'
 
 
 class App extends Component {
@@ -34,6 +34,7 @@ class App extends Component {
                 <ol className="todoList">
                   {todos}
                 </ol>
+                <UserDialog />
             </div>
             )
 }
