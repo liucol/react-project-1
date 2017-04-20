c133fd16d5771c3ede99f61dc11d2d4312b9bce1
杨柳 <1545766219@qq.com>
2017-04-20 16:34:00 +0800

    注册成功后。记住user,显示username

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
   constructor(props){
         super(props)
         this.state = {
+              user: {},
               newTodo: ' ',
               todoList:[]
         }
@@ -27,17 +28,21 @@ class App extends Component {
 
         return (
             <div className="App">
-                <h1>我的待办</h1>
+                <h1>{this.state.user.username||'我'}的待办</h1>
                 <div className="inputWrapper">
                       <TodoInput content={this.state.newTodo} onChange={this.changeTitle.bind(this)} onSubmit={this.addTodo.bind(this)}/>
                 </div>
                 <ol className="todoList">
                   {todos}
                 </ol>
-                <UserDialog />
+                <UserDialog  onSignUp={this.onSignUp.bind(this)}/>
             </div>
             )
 }
+    onSignUp(user){
+        this.state.user = user
+        this.setState(this.state)
+    }
     componentDidUpdate(){
     }
     toggle(e, todo){
