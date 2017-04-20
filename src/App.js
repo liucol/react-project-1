2bf8d1890096fb30868a029e52d3314282e03ae8
杨柳 <1545766219@qq.com>
2017-04-20 17:24:24 +0800

    添加登出功能

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import UserDialog from './UserDialog'
-import {getCurrentUser} from './leanCloud'
+import {getCurrentUser, signOut} from './leanCloud'
 
 class App extends Component {
     constructor(props) {
@@ -29,7 +29,9 @@ class App extends Component {
 
         return (
             <div className="App">
-                <h1>{this.state.user.username || '我'}的待办</h1>
+                <h1>{this.state.user.username || '我'}的待办
+                    {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
+                </h1>
                 <div className="inputWrapper">
                     <TodoInput content={this.state.newTodo} onChange={this.changeTitle.bind(this)}
                                onSubmit={this.addTodo.bind(this)}/>
@@ -42,6 +44,12 @@ class App extends Component {
         )
     }
 
+    signOut(){
+            signOut()
+            let stateCopy = JSON.parse(JSON.stringify(this.state))
+           stateCopy.user = {}
+              this.setState(stateCopy)
+            }
     onSignUp(user) {
         let stateCopy = JSON.parse(JSON.stringify(this.state))
         stateCopy.user = user
